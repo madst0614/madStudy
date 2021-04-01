31e081d8247535d142440902e3d38f39ae16b01e
madst0614 <seung@DESKTOP-6IGFRUI>
2021-04-02 02:29:33 +0900

    studyhard

--- src/stage2/Problem1330.java
+++ src/stage2/Problem1330.java
@@ -1,10 +1,22 @@
 package stage2;
+import java.util.Scanner;
 
 public class Problem1330 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		
+		a=sc.nextInt();
+		b=sc.nextInt();
+		
+		if(a<b)
+			System.out.println("<");
+		else if(a==b)
+			System.out.println("=");
+		else
+			System.out.println(">");
+		
 	}
-
 }
