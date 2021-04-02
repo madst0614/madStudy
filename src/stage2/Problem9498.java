928a67b2f21b0cea71ffbcd3665fe65553184d5b
madst0614 <madst0614@gmail.com>
2021-04-03 02:07:57 +0900

    studyhard

---
+++ src/stage2/Problem9498.java
@@ -0,0 +1,24 @@
+package stage2;
+import java.util.Scanner;
+
+public class Problem9498 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a;
+		a=sc.nextInt();
+		sc.close();
+		if(a>=90&&a<=100)
+			System.out.println("A");
+		else if(a>=80 && a<90)
+			System.out.println("B");
+		else if(a>=70 && a<80)
+			System.out.println("C");
+		else if(a>=60 && a<70)
+			System.out.println("D");
+		else
+			System.out.println("F");
+	}
+
+}
