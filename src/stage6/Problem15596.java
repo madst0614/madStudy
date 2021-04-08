1ae998cd566dcc7c973e254cefba0326aca4c266
madst0614 <madst0614@gmail.com>
2021-04-09 05:06:19 +0900

    hard

---
+++ src/stage6/Problem15596.java
@@ -0,0 +1,17 @@
+package stage6;
+
+//public class Test {
+//    long sum(int[] a) {
+//        long ans = 0;
+//        for(int i=0; i<a.length; i++)
+//        	ans+=(long)a[i];
+//        return ans;
+//    }
+//}
+
+public class Problem15596 {
+
+	public static void main(String[] args){
+	    
+	    }
+}
---
+++ src/stage6/Problem4673.java
@@ -0,0 +1,14 @@
+package stage6;
+
+public class Problem4673 {
+	
+	public static int selfNumber(int n) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
